0074d0d096cf8f99e97ec3f31f3888ed3327546b
Mehmet Köl Tiğin <102043225+koltigin@users.noreply.github.com>
2024-02-05 04:30:26 +0300

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -46,7 +46,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/koltigin/web-test/tree/main',
         },
         blog: false,
         theme: {
